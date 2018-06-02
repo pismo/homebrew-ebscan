fb59a8791a5289a59d2c5b15ad2e1cfac3191dd4
andriosr <andriosr@gmail.com>
2018-06-02 11:13:35 -0300

    Brew formula update for homebrew-ebscan version v0.1.1

--- Formula/ebscan.rb
+++ Formula/ebscan.rb
@@ -1,9 +1,9 @@
 class Ebscan < Formula
   desc "manipulates Elasticbeanstalk environmnet variables for all environments at once."
-  homepage "https://github.com/pismo/ebscan/"
-  url "https://github.com/pismo/ebscan/releases/download/v0.1.3/ebscan_0.1.3_macOS_64-bit.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.1.3"
-  sha256 "86e67fb8e6c7772a7c8b0268a61c4636e095cd5301a3482bf66969677c79d62b"
+  homepage "https://github.com/pismo/homebrew-ebscan/"
+  url "https://github.com/pismo/homebrew-ebscan/releases/download/v0.1.1/homebrew-ebscan_0.1.1_macOS_64-bit.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.1.1"
+  sha256 "e87d9a290ce9736bae6f47868021c08410f8c5fe18ff5b4aa5ea44956bd6a7e5"
   
   depends_on "awsebcli"
 
