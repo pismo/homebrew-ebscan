8b6f3781998a151774afd24563f6f9a67458cc4e
andriosr <andriosr@gmail.com>
2018-06-04 11:15:58 -0300

    Brew formula update for homebrew-ebscan version v0.1.7

--- Formula/ebscan.rb
+++ Formula/ebscan.rb
@@ -1,18 +1,18 @@
 class Ebscan < Formula
   desc "manipulates Elasticbeanstalk environmnet variables for all environments at once."
   homepage "https://github.com/pismo/homebrew-ebscan/"
-  url "https://github.com/pismo/homebrew-ebscan/releases/download/v0.1.1/homebrew-ebscan_0.1.1_macOS_64-bit.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.1.1"
-  sha256 "e87d9a290ce9736bae6f47868021c08410f8c5fe18ff5b4aa5ea44956bd6a7e5"
+  url "https://github.com/pismo/homebrew-ebscan/releases/download/v0.1.7/homebrew-ebscan_0.1.7_macOS_64-bit.tar.gz"
+  version "0.1.7"
+  sha256 "6fe080ddad24c6b3a00e0a8da631cbf4b1c14538790cad54241dcce3e44c91b1"
   
   depends_on "awsebcli"
 
   def install
-    bin.install "ebscan"
+    bin.install "homebrew-ebscan"
   end
 
   def caveats; <<~EOS
-    ebscan -contains `substring-in-env-var`
+    homebrew-ebscan -contains `substring-in-env-var`
   EOS
   end
 end
